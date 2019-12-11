81f7864a4c048a1f589e6ee308c41e2a4150ce8e
binyot <sxterminal@protonmail.com>
2019-12-11 19:40:02 +0300

    added MarginItemDecorator

---
+++ app/src/main/java/com/miemdynamics/fossbot/ui/decorator/MarginItemDecorator.kt
@@ -0,0 +1,23 @@
+package com.miemdynamics.fossbot.ui.decorator
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MarginItemDecorator(private val space: Int): RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        with(outRect) {
+            if (parent.getChildAdapterPosition(view) == 0) {
+                top = space
+            }
+            left = space
+            right = space
+            bottom = space
+        }
+    }
+}
