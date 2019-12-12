a8b2e98d42cdec8bfe4e3677251ff705e446958e
binyot <sxterminal@protonmail.com>
2019-12-12 11:03:54 +0300

    fexed broken import

--- app/src/main/java/com/miemdynamics/fossbot/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/miemdynamics/fossbot/ui/settings/SettingsFragment.kt
@@ -3,7 +3,6 @@ package com.miemdynamics.fossbot.ui.settings
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.miemdynamics.fossbot.R
-import com.miemdynamics.fossbot.internal.intPreference
 
 /**
  * A special root [PreferenceFragmentCompat] for user preferences.
