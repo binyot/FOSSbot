12a06ffa4b27dc6bbe8177724a835fb0476eb376
binyot <sxterminal@protonmail.com>
2019-12-13 15:04:14 +0300

    added DiffUtil for program list

--- app/src/main/java/com/miemdynamics/fossbot/ui/program/ProgramAdapter.kt
+++ app/src/main/java/com/miemdynamics/fossbot/ui/program/ProgramAdapter.kt
@@ -3,18 +3,18 @@ package com.miemdynamics.fossbot.ui.program
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.miemdynamics.fossbot.R
 import com.miemdynamics.fossbot.data.entity.Program
 import kotlinx.android.synthetic.main.program_item.view.*
 
 class ProgramAdapter: RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
-
-    // TODO: Use DiffUtil
     var programList: List<Program> = ArrayList()
         set(programList) {
+            val diff = DiffUtil.calculateDiff(ProgramDiffCallback(field, programList))
             field = programList
-            notifyDataSetChanged()
+            diff.dispatchUpdatesTo(this)
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgramViewHolder {
@@ -35,5 +35,22 @@ class ProgramAdapter: RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
     class ProgramViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val textViewName = view.textViewName
         val textViewBody = view.textViewSource
+    }
+
+    class ProgramDiffCallback(
+        private val old: List<Program>,
+        private val new: List<Program>
+    ): DiffUtil.Callback() {
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            old.get(oldItemPosition) == new.get(newItemPosition)
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            old.get(oldItemPosition).name == new.get(newItemPosition).name
+
+        override fun getNewListSize() =
+            new.size
+
+        override fun getOldListSize() =
+            old.size
     }
 }
